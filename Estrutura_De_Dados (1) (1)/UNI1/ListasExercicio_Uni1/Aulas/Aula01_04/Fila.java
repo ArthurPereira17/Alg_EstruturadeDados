c1a8ad96f042d1ee1fb8c01c7c6139c8f3a649f8
Arthur Nascimento Pereira <arthurnapereira@gmail.com>
2026-05-12 14:19:19 -0300

    Delete Estrutura_De_Dados (1) (1) directory

--- Estrutura_De_Dados (1) (1)/UNI1/ListasExercicio_Uni1/Aulas/Aula01_04/Fila.java
+++
@@ -1,9 +0,0 @@
-package Aulas.Aula01_04;
-
-public interface Fila<T> {
-    void inserir(T valor);
-    boolean estaVazia();
-    T peek();
-    T retirar();
-    void liberar();
-}
--- Estrutura_De_Dados (1) (1)/UNI1/ListasExercicio_Uni1/L06/Fila.java
+++
@@ -1,9 +0,0 @@
-package L06;
-
-public interface Fila<T> {
-    void inserir(T valor);
-    boolean estaVazia();
-    T peek();
-    T retirar();
-    void liberar();
-}
