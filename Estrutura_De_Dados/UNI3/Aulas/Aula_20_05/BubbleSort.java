fe45d4a42f19bf965d1ddccf05415e0f5309652b
ArthurPereira17 <anpereira@furb.br>
2026-05-22 03:34:18 -0300

    Classe dos metodos de ordenação completas

--- Estrutura_De_Dados/UNI3/Aulas/Aula_20_05/BubbleSort.java
+++ Estrutura_De_Dados/UNI3/Aulas/Aula_20_05/BubbleSort.java
@@ -1,5 +1,17 @@
 package Aulas.Aula_20_05;
 
-public class BubbleSort {
-    
+public class BubbleSort<T extends Comparable<T>> extends OrdenacaoAbstract<T> {
+
+    @Override
+    public void ordenar() {
+        int n = getInfo().length;
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 0; j <= i - 1; j++) {
+                if (getInfo()[j].compareTo(getInfo()[j + 1]) > 0) {
+                    trocar(j, j + 1);
+                }
+            }
+        }
+    }
 }
--- Estrutura_De_Dados/UNI3/Aulas/Aula_20_05/MergeSort.java
+++ Estrutura_De_Dados/UNI3/Aulas/Aula_20_05/MergeSort.java
@@ -1,5 +1,53 @@
 package Aulas.Aula_20_05;
 
-public class MergeSort {
+public class MergeSort<T extends Comparable<T>> extends OrdenacaoAbstract<T> {
 
-}
+    @Override
+    public void ordenar(){
+        int n = getInfo().length - 1;
+        mergeSort(0, n);
+    }
+
+    private void mergeSort(int inicio, int fim){
+        if(inicio < fim){
+            int meio = (inicio + fim) / 2;
+            mergeSort(inicio, meio);
+            mergeSort(meio + 1, fim);
+            merge(inicio, fim, meio);
+        }
+    }
+
+    private void merge(int inicio, int fim, int meio){
+        int tamEsquerda = meio - inicio + 1;
+        T[] esquerda = (T[]) new Comparable[tamEsquerda];
+        for(int i = 0; i < tamEsquerda; i++){
+            esquerda[i] = getInfo()[inicio + 1];
+        }
+
+        int tamDireita = fim - meio;
+        T[] direira = (T[]) new Comparable[tamDireita];
+        for(int i = 0; i < tamDireita; i++){
+            direita[i] = getInfo()[meio + 1 + i];
+        }
+
+        int cEsq = 0;
+        int cDir = 0;
+        int i = inicio;
+
+        while(cEsq < tamEsquerda && cDir < tamDireita){
+            if (esquerda[cEsq].compareTo(direita[cDir]) <= 0){
+                getInfo()[i] = esquerda[cEsq++];
+            } else{
+                getInfo()[i] = direita[cDir++];
+            }
+            i++;
+            }
+
+            while (cEsq < tamEsquerda){
+                getInfo()[i++] = esquerda[cEsq++];
+            }
+            while (cDir < tamDireita){
+                getInfo()[i++] = direita[cDir++];
+            }
+        }
+    }
