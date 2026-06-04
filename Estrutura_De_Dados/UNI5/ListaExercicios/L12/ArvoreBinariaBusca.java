b67b95ff288e7ee4959bff8e3635f20c7ebb4564
Arthur Nascimento Pereira <arthurnapereira@gmail.com>
2026-06-03 21:10:30 -0300

    Adicionando método de remover

--- Estrutura_De_Dados/UNI5/ListaExercicios/L12/ArvoreBinariaBusca.java
+++ Estrutura_De_Dados/UNI5/ListaExercicios/L12/ArvoreBinariaBusca.java
@@ -1,74 +1,121 @@
 package UNI5.ListaExercicios.L12;
 
-public class ArvoreBinariaBusca<T extends Comparable<T>>
-        extends ArvoreBinariaAbstract<T> {
+public class ArvoreBinariaBusca<T extends Comparable<T>> extends ArvoreBinariaAbstract<T> {
 
     public void inserir(T info) {
-        setRaiz(inserir(getRaiz(), info));
+        NoArvoreBinaria<T> novoNo = new NoArvoreBinaria<>(info);
+        if (estaVazia()) {
+            setRaiz(novoNo);
+        } else {
+            inserirRecursivo(getRaiz(), novoNo);
+        }
     }
 
-    private NoArvoreBinaria<T> inserir(NoArvoreBinaria<T> no, T info) {
-        if (no == null) {
-            return new NoArvoreBinaria<>(info);
+    private void inserirRecursivo(NoArvoreBinaria<T> atual, NoArvoreBinaria<T> novo) {
+        if (novo.getInfo().compareTo(atual.getInfo()) < 0) {
+            if (atual.getEsquerda() == null) {
+                atual.setEsquerda(novo);
+            } else {
+                inserirRecursivo(atual.getEsquerda(), novo);
+            }
+        } else {
+            if (atual.getDireita() == null) {
+                atual.setDireita(novo);
+            } else {
+                inserirRecursivo(atual.getDireita(), novo);
+            }
         }
-
-        int comparacao = info.compareTo(no.getInfo());
-
-        if (comparacao < 0) {
-            no.setEsquerda(inserir(no.getEsquerda(), info));
-        } else if (comparacao > 0) {
-            no.setDireita(inserir(no.getDireita(), info));
-        }
-
-        return no;
     }
 
     @Override
     public NoArvoreBinaria<T> buscar(T info) {
-        return buscar(getRaiz(), info);
+        return buscarRecursivo(getRaiz(), info);
     }
 
-    private NoArvoreBinaria<T> buscar(NoArvoreBinaria<T> no, T info) {
+    private NoArvoreBinaria<T> buscarRecursivo(NoArvoreBinaria<T> no, T info) {
         if (no == null) return null;
 
         int comparacao = info.compareTo(no.getInfo());
-
         if (comparacao == 0) return no;
-        if (comparacao < 0) return buscar(no.getEsquerda(), info);
-        return buscar(no.getDireita(), info);
+        else if (comparacao < 0) return buscarRecursivo(no.getEsquerda(), info);
+        else return buscarRecursivo(no.getDireita(), info);
     }
 
-    public void remover(T info) {
-        setRaiz(remover(getRaiz(), info));
-    }
+public void retirar(T valor) {
+        if (estaVazia()) return;
 
-    private NoArvoreBinaria<T> remover(NoArvoreBinaria<T> no, T info) {
-        if (no == null) return null;
+        NoArvoreBinaria<T> p = getRaiz();
+        NoArvoreBinaria<T> pai = null;
+        boolean filhoEsquerda = false;
 
-        int comparacao = info.compareTo(no.getInfo());
-
-        if (comparacao < 0) {
-            no.setEsquerda(remover(no.getEsquerda(), info));
-        } else if (comparacao > 0) {
-            no.setDireita(remover(no.getDireita(), info));
-        } else {
-            if (no.getEsquerda() == null) return no.getDireita();
-
-            if (no.getDireita() == null) return no.getEsquerda();
-
-
-            NoArvoreBinaria<T> sucessor = menorNo(no.getDireita());
-            no.setInfo(sucessor.getInfo());
-            no.setDireita(remover(no.getDireita(), sucessor.getInfo()));
+        while (p != null && !p.getInfo().equals(valor)) {
+            pai = p;
+            if (valor.compareTo(p.getInfo()) < 0) {
+                filhoEsquerda = true;
+                p = p.getEsquerda();
+            } else {
+                filhoEsquerda = false;
+                p = p.getDireita();
+            }
         }
 
-        return no;
+        if (p == null) return;
+
+        if (p.getEsquerda() == null && p.getDireita() == null) {
+            if (p == getRaiz()) {
+                setRaiz(null);
+            } else if (filhoEsquerda) {
+                pai.setEsquerda(null);
+            } else {
+                pai.setDireita(null);
+            }
+        } 
+        else if (p.getEsquerda() == null || p.getDireita() == null) {
+            NoArvoreBinaria<T> filho = (p.getEsquerda() != null) ? p.getEsquerda() : p.getDireita();
+
+            if (p == getRaiz()) {
+                setRaiz(filho);
+            } else if (filhoEsquerda) {
+                pai.setEsquerda(filho);
+            } else {
+                pai.setDireita(filho);
+            }
+        } 
+        else {
+            NoArvoreBinaria<T> sucessor = extrairSucessor(p);
+
+            if (p == getRaiz()) {
+                setRaiz(sucessor);
+            } else if (filhoEsquerda) {
+                pai.setEsquerda(sucessor);
+            } else {
+                pai.setDireita(sucessor);
+            }
+
+            sucessor.setEsquerda(p.getEsquerda());
+        }
     }
 
-    private NoArvoreBinaria<T> menorNo(NoArvoreBinaria<T> no) {
-        while (no.getEsquerda() != null) {
-            no = no.getEsquerda();
+    private NoArvoreBinaria<T> extrairSucessor(NoArvoreBinaria<T> p) {
+        NoArvoreBinaria<T> paiSucessor = p;
+        NoArvoreBinaria<T> sucessor = p.getDireita();
+        NoArvoreBinaria<T> atual = p.getDireita();
+
+        while (atual != null) {
+            paiSucessor = sucessor;
+            sucessor = atual;
+            atual = atual.getEsquerda();
         }
-        return no;
+
+        if (sucessor != p.getDireita()) {
+            paiSucessor.setEsquerda(sucessor.getDireita());
+            sucessor.setDireita(p.getDireita());
+        }
+
+        return sucessor;
+    }
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
